0f0fe2ceeb9ce06d41bfb1569c8ed7a020d75706
Trenton Smith <tlsdude@gmail.com>
2015-02-05 00:51:27 -0700

    First commit with q.h complete, but not tested

---
+++ Project1/q.h
@@ -0,0 +1,53 @@
+#ifndef q
+#define q
+#include <stdlib.h>
+
+typedef struct queue queue;
+
+struct queue {
+	queue *prev;
+	queue *next;
+	int payload;
+};
+
+queue *NewItem() {
+	return malloc(sizeof(queue));
+}
+
+void FreeItem(queue *item) {
+	free(item);
+}
+
+void InitQueue(queue *head) {
+	head = NewItem();
+	head->next = head;
+	head->prev = head;
+}
+
+void AddQueue(queue *head, queue *item) {
+	queue *last = head;
+	while (last->next != head) {
+		last = last->next;
+	}
+	item->prev = last;
+	item->next = head;
+	last->next = item;	
+	head->prev = item;
+}
+
+queue *DelQueue(queue *head) {
+	queue *delete = head;
+	while (delete->next != head) {
+		delete = delete->next;
+	}
+	delete->prev->next = delete->next;
+	delete->next->prev = delete->prev;
+	return delete;
+}
+
+void RotateQ(queue *head) {
+	queue *temp = head;
+	head = temp->next;
+}	
+
+#endif
