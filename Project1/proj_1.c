adde5a5dce6ce9f9bb8792f69d2b69cb31481c39
Trenton Smith <tlsdude@gmail.com>
2015-02-10 17:45:05 -0700

    pushing fixes

--- Project1/proj_1.c
+++ Project1/proj_1.c
@@ -1,5 +1,66 @@
 #include "q.h"
 
 int main() {
+	puts("begin\n");
+	queue *head=NULL;
+	printf("null head %p\n",head);
+	
+	InitQueue(&head);
+	printf("initialized head %p\n",head);
 
+	//create and add new elements
+	puts("\ncreating elements");
+	queue *q = NewItem();
+	queue *q2 = NewItem();
+	queue *q3 = NewItem();
+	q->payload = 1;
+	q2->payload = 2;
+	q3->payload = 3;
+	printf("q: %p : %d\n", q, q->payload);
+	printf("q2: %p : %d\n", q2, q2->payload);
+	printf("q3: %p : %d\n", q3, q3->payload);
+
+	puts("\nadding elements");
+	AddQueue(&head, q);
+	q = NULL;
+	PrintQueue(head);
+	puts("\n");
+	AddQueue(&head, q2);
+	q2 = NULL;
+	PrintQueue(head);
+	puts("\n");
+	AddQueue(&head, q3);
+	q3 = NULL;
+	PrintQueue(head);
+	
+	puts("\nrotating elements");
+	RotQueue(&head);
+	PrintQueue(head);
+	puts("");
+	RotQueue(&head);
+	PrintQueue(head);
+	puts("");
+	RotQueue(&head);
+	PrintQueue(head);
+
+	//free all elements
+	puts("\ndeleting elements");
+	queue* temp = DelQueue(&head);
+	printf("delete: %p\t %d\n", temp, temp->payload);
+	FreeItem(&temp);
+	printf("deleted: %p\n", temp);
+	PrintQueue(head);
+	puts("\n");
+	temp = DelQueue(&head);
+	printf("delete: %p\t %d\n", temp, temp->payload);
+	FreeItem(&temp);
+	PrintQueue(head);
+	puts("\n");
+	temp = DelQueue(&head);
+	printf("delete: %p\t %d\n", temp, temp->payload);
+	FreeItem(&temp);
+	PrintQueue(head);
+	
+	puts("end");
+	return 0;
 }
--- Project1/q.h
+++ Project1/q.h
@@ -1,53 +1,85 @@
-#ifndef queue
-#define queue
+#ifndef QUEUE_HEADER
+#define QUEUE_HEADER
 #include <stdlib.h>
 
-typedef struct queue queue;
-
-struct queue {
-	queue *prev;
-	queue *next;
+typedef struct queue {
+	struct queue* prev;
+	struct queue* next;
 	int payload;
-};
+} queue;
 
-queue *NewItem() {
-	return malloc(sizeof(queue));
+queue* NewItem() {
+	return (queue*)malloc(sizeof(queue));
 }
 
-void FreeItem(queue *item) {
-	free(item);
+void FreeItem(queue** item) {
+	free(*item);
+	*item = NULL;
 }
 
-void InitQueue(queue *head) {
-	head = NewItem();
-	head->next = head;
-	head->prev = head;
+void InitQueue(queue** head) {
+	*head = NewItem();
+	(*head)->next = NULL;
+	(*head)->prev = NULL;
+	(*head)->payload = -1;
 }
 
-void AddQueue(queue *head, queue *item) {
-	queue *last = head;
-	while (last->next != head) {
-		last = last->next;
+void AddQueue(queue** head, queue* item) {
+	queue* temp = *head;
+	if (temp->next == NULL) {
+		temp->payload = item->payload;
+		temp->next = temp;
+		temp->prev = temp;
+		FreeItem(&item);		
+	} else if (temp->next == temp) {		
+		temp->next = item;
+		temp->prev = item;
+		item->next = temp;
+		item->prev = temp;
+	} else {
+		while (temp->next != *head)
+			temp = temp->next;
+		item->next = temp->next;
+		item->prev = temp;
+		temp->next = item;
+		(*head)->prev = item;
 	}
-	item->prev = last;
-	item->next = head;
-	last->next = item;	
-	head->prev = item;
 }
 
-queue *DelQueue(queue *head) {
-	queue *delete = head;
-	while (delete->next != head) {
-		delete = delete->next;
+queue* DelQueue(queue** head) {
+	queue* delete = *head;
+	if (delete->next == delete) {
+		*head = NULL;
+	} else {
+		while (delete->next != *head) {
+			delete = delete->next;
+		}
+		delete->prev->next = delete->next;
+		delete->next->prev = delete->prev;
 	}
-	delete->prev->next = delete->next;
-	delete->next->prev = delete->prev;
 	return delete;
 }
 
-void RotateQ(queue *head) {
-	queue *temp = head;
-	head = temp->next;
+void RotQueue(queue** head) {
+	queue* temp = *head;
+	*head = temp->next;
 }	
+
+void PrintQueue(queue* head) {
+	if (head == NULL) {
+		puts("Empty queue");
+		return;
+	}
+	printf("queue:");
+	if ((head)->next == head) {
+		printf("\t%p : %d\n", head, head->payload);
+	} else {
+		queue* last = head;
+		do {
+			printf("\t%p : %d\n", last, last->payload);
+			last = last->next;
+		} while (last != head);
+	}
+}
 
 #endif
